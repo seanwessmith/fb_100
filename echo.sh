ab534b47877f46f22a9b054b4d278a6b2abea9dc
Sean Smith <sean@Seans-MacBook-Pro.local>
2015-07-23 04:19:45 -0400

    Working, cleaned up

--- echo.sh
+++ echo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((n=0;n<10;n++))
+for ((n=1;n<101;n++))
 do
-curl -L http://www.facebook.com/$n | grep "pageTitle" | awk '{sub(/.*title id="pageTitle">/,"");sub(/F.*/,"");print;}' >> output27.txt
+curl -L http://www.facebook.com/$n | grep "pageTitle" | awk '{sub(/.*title id="pageTitle">/,"");sub(/F.*/,"");print;}' >> output.txt
 done
