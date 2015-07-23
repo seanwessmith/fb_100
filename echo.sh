db62a8925e2c0cd8112a59aca93e16f86a0a9f0b
Sean Smith <sean@Seans-MacBook-Pro.local>
2015-07-23 04:30:14 -0400

    ask for filename

--- echo.sh
+++ echo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Pls enter your filename:"
+read filename
 for ((n=1;n<101;n++))
 do
-curl -L http://www.facebook.com/$n | grep "pageTitle" | awk '{sub(/.*title id="pageTitle">/,"");sub(/F.*/,"");print;}' >> output.txt
+curl -L http://www.facebook.com/$n | grep "pageTitle" | awk '{sub(/.*title id="pageTitle">/,"");sub(/F.*/,"");print;}' >> $filename.txt
 done
