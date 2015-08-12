a628c9782fa6957c097fcbcc7b142dc642121e77
Sean Smith <seanwessmith@gmail.com>
2015-08-11 20:12:47 -0400

    updated number of users

--- fb.sh
+++ fb.sh
@@ -4,7 +4,7 @@
 echo "Pls enter your filename:"
 read filename
 
-for ((n=1;n<5;n++))
+for ((n=1;n<101;n++))
 do
 t=$(curl -sL http://www.facebook.com/$n | grep "pageTitle" | awk '{sub(/.*title id="pageTitle">/,"");sub(/F.*/,"");print;}')
 echo "$n $t" >> $filename.txt
