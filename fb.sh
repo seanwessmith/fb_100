cc4e894cc76d1bfd696c3480bc773b021e520ddf
Sean Smith <seanwessmith@gmail.com>
2015-08-11 20:22:52 -0400

    Update fb.sh

--- fb.sh
+++ fb.sh
@@ -8,7 +8,6 @@ for ((n=1;n<101;n++))
 do
 t=$(curl -sL http://www.facebook.com/$n | grep "pageTitle" | awk '{sub(/.*title id="pageTitle">/,"");sub(/F.*/,"");print;}')
 echo "$n $t" >> $filename.txt
-p=n/101
 
 bar $((n));
 done
