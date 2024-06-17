5931bc92e3c8c094788528fd6782fc13cef08e9f
Sean Smith <seanwessmith@gmail.com>
2024-06-16 20:47:29 -0700

    fixed bug, facebook requires a cookie now

--- README.md
+++ README.md
@@ -6,14 +6,17 @@ Grab first ever 100 Facebook users
 1. Click on the download zip link to the right of the page. 
 2. Drag the newly downloaded folder (fb_100-master) to your desktop.
 3. Open the Spotlight search field (cmd + spacebar) and search for the Terminal application.
-4. Press the following to navigate to your desktop
+4. Copy your cookie from a facebook network request. Right click a facebook page, select "Inspect", click the "Network" tab. Filter by "Doc" requests by clicking the "Doc" button. Click a request in the table. Scroll down to find the "Cookie:" key and copy the value.
+![alt tag](https://github.com/seanwessmith/fb_100/blob/master/network_tab_cookie.png)
+6. Save the cookie value in the .env file
+7. Press the following to navigate to your desktop
 ```bash
 cd desktop
 cd fb_100-master
 ./fb.sh
 ```
-5. Type in a name for the output file and press enter. 
-6. The program will execute and tell you when it is done. 
-7. Open the fb_100 folder on your desktop. 
-8. Double click the text file you just named. 
-9. You are now done.
+1. Type in a name for the output file and press enter. 
+2. The program will execute and tell you when it is done. 
+3. Open the fb_100 folder on your desktop. 
+4. Double click the text file you just named. 
+5. You are now done.
--- fb.sh
+++ fb.sh
@@ -1,14 +1,58 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-. bar.sh
-echo "Pls enter your filename:"
-read filename
+# Load environment variables from .env file
+eval "$(
+    awk -F= '{
+        if (!/^\s*#/ && !/^\s*$/) {
+            key=$1;
+            sub(/^ *| *$/, "", key);
+            value=substr($0, index($0,$2));
+            sub(/^ *| *$/, "", value);
+            print "export " key "=\"" value "\""
+        }
+    }' .env
+)"
 
-for ((n=1;n<101;n++))
-do
-t=$(curl -sL http://www.facebook.com/$n | grep "pageTitle" | awk '{sub(/.*title id="pageTitle">/,"");sub(/F.*/,"");print;}')
-echo "$n $t" >> $filename.txt
+# Source the progress bar script
+. progress_bar.sh
 
-bar $((n));
+# Prompt for filename
+echo "Please enter your filename:"
+read -r filename
+
+# Loop to fetch data from Facebook
+for ((n = 1; n <= 100; n++)); do
+    # Fetch HTML content
+    html=$(curl -sL "https://www.facebook.com/$n" \
+        -H "cookie: $FB_COOKIE" \
+        -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+        -H 'accept-language: en-US,en;q=0.9,ru;q=0.8' \
+        -H 'cache-control: max-age=0' \
+        -H 'dpr: 2' \
+        -H 'priority: u=0, i' \
+        -H 'sec-ch-prefers-color-scheme: dark' \
+        -H 'sec-ch-ua: "Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"' \
+        -H 'sec-ch-ua-full-version-list: "Google Chrome";v="125.0.6422.142", "Chromium";v="125.0.6422.142", "Not.A/Brand";v="24.0.0.0"' \
+        -H 'sec-ch-ua-mobile: ?0' \
+        -H 'sec-ch-ua-model: ""' \
+        -H 'sec-ch-ua-platform: "macOS"' \
+        -H 'sec-ch-ua-platform-version: "15.0.0"' \
+        -H 'sec-fetch-dest: document' \
+        -H 'sec-fetch-mode: navigate' \
+        -H 'sec-fetch-site: same-origin' \
+        -H 'sec-fetch-user: ?1' \
+        -H 'upgrade-insecure-requests: 1' \
+        -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36' \
+        -H 'viewport-width: 1440')
+
+    # Extract headline using awk
+    name=$(echo "$html" | sed -n '/"data":{"name":"\([^"]*\)","gender"/s/.*"data":{"name":"\([^"]*\)","gender".*/\1/p')
+
+    # Save extracted headline to summary file
+    echo "$n $name" >>"$filename.txt"
+
+    # Update progress bar
+    progress_bar $((n))
 done
+
 echo "Congratulations on finding what's left of the first 100 FB users!"
